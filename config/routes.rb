c642a1444a603512ce6744ef7130ba296d69025c
Yutaka8842 <inoue.8842@gmail.com>
2021-07-21 21:59:32 +0900

    ファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
+  get 'info',  to: 'main#info'
+  get 'pricelist',  to: 'main#pricelist'
+  get 'contact',  to: 'main#contact'
 end
